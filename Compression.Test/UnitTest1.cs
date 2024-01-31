30bc8679725f828a1da6c7fa4d862809d8816f05
Wayne Blaylock <wayne.blaylock1324@gmail.com>
2024-01-30 19:28:52 -0700

    added tests

--- Compression.Test/UnitTest1.cs
+++ Compression.Test/UnitTest1.cs
@@ -8,8 +8,20 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void FindBiggest()
     {
-        Assert.Pass();
+        Assert.That(Compressor.FindBiggest("1"), Is.EqualTo(0));
+        Assert.That(Compressor.FindBiggest("11"), Is.EqualTo(1));
+        Assert.That(Compressor.FindBiggest("100"), Is.EqualTo(0));
+        Assert.That(Compressor.FindBiggest("1011101"), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void CompressString()
+    {
+        Assert.That(Compressor.CompressString("11"), Is.EqualTo(("1",(1,1))));
+        Assert.That(Compressor.CompressString("1010"), Is.EqualTo(("10",(2,2))));
+        Assert.That(Compressor.CompressString("100101"), Is.EqualTo(("10",(2,3))));
+        Assert.That(Compressor.CompressString("110101011101"), Is.EqualTo(("11010101",(4,8))));
     }
 }
--- Compression/Class1.cs
+++ Compression/Class1.cs
@@ -7,4 +7,16 @@ public class Compressor : IBitStringCompressor{
     public static string Decompression(){
         return "hello";
         }
+    public static 
+}
+
+public class GenerateString{
+    public static void GetString(int length){
+        Random random = new Random();
+        string randstring = "";
+        for (int i = 0; i < length; i++){
+            randstring += random.Next(0,2);
+        }
+    Console.WriteLine(randstring);
+    }
 }

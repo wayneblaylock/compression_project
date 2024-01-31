3b614fa263cd022aa1eef0b34ce5f6f7ba7daebe
AAtuan <abeleatuan@gmail.com>
2024-01-30 19:37:56 -0700

    FindBiggest

--- Compression/Class1.cs
+++ Compression/Class1.cs
@@ -1,21 +1,54 @@
 ﻿namespace Compression;
 
-public class Compressor : IBitStringCompressor{
-    public static string Compression(){
+public class Compressor : IBitStringCompressor
+{
+    public static string Compression()
+    {
         return "hello";
     }
-    public static string Decompression(){
+    public static string Decompression()
+    {
         return "hello";
-        }
+    }
 }
 
-public class GenerateString{
-    public static void GetString(int length){
+public class GenerateString
+{
+    public static void GetString(int length)
+    {
         Random random = new Random();
         string randstring = "";
-        for (int i = 0; i < length; i++){
-            randstring += random.Next(0,2);
+        for (int i = 0; i < length; i++)
+        {
+            randstring += random.Next(0, 2);
         }
-    Console.WriteLine(randstring);
+        Console.WriteLine(randstring);
+    }
+}
+
+public class CompressorHelper
+{
+    public static int FindBiggest(string binaryString)
+    {
+        int maxLength = 0;
+        int length = 1;
+
+        for (int i = 1; i < binaryString.Length; i++)
+        {
+            if (binaryString[i] == binaryString[i - 1])
+            {
+                length++;
+                if (length > maxLength)
+                {
+                    maxLength = length;
+                }
+            }
+            else
+            {
+                length = 1;
+            }
+        }
+
+        return maxLength;
     }
 }

6b23338fbefb1dd20ccb3b389637e0ff345aca9c
Wayne Blaylock <wayne.blaylock1324@gmail.com>
2024-01-31 18:38:35 -0700

    working until bottleneck

--- Compression.Main/Program.cs
+++ Compression.Main/Program.cs
@@ -1,3 +1,14 @@
 ﻿using Compression;
 
-CompressorHelper.FindBiggest("1011101");
+
+// string bits = "10111011001011010101011101";
+// Console.WriteLine(bits);
+// (int,int) instructions = CompressorHelper.FindBiggest(bits);
+// Console.WriteLine(instructions.ToString());
+// string compressedBits = CompressorHelper.CompressString(bits, instructions);
+// Console.WriteLine(compressedBits);
+
+string bits = GenerateString.GetString(10);
+Console.WriteLine(bits);
+(string compressed, List<(int,int)>) output = Compressor.Compression(bits);
+Console.WriteLine(output.compressed);
